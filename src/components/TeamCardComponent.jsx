f3c15d0644c83d492fdc88743a535c1546d499ae
nandhu-44 <nandhu90040@gmail.com>
2024-06-26 14:51:19 +0530

    Team UI reponsive update

--- src/components/TeamCardComponent.jsx
+++ src/components/TeamCardComponent.jsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import { FaLinkedin } from "react-icons/fa6";
 import { Poppins } from "next/font/google";
+import Link from "next/link";
 
 const poppins = Poppins({
   weight: ["400", "700"],
@@ -11,14 +14,18 @@ const poppins = Poppins({
 const TeamCardComponent = ({ imageSrc, name, role, linkedInURL }) => {
   return (
     <div
-      className={`my-12 flex w-64 flex-col items-center rounded-md bg-[#77B0AA] p-4 text-[#111111] ${poppins.variable}`}
+      className={`my-8 flex w-[300px] flex-col items-center rounded-md bg-[#77B0AA] p-4 text-[#111111] md:w-80 lg:w-96 ${poppins.variable}`}
     >
-      <Image src={imageSrc} alt="" className="mt-3 size-32 rounded-full" />
-      <h3 className="mt-2 text-3xl font-bold">{name}</h3>
-      <p className="mt-2">{role}</p>
-      <a href={linkedInURL}>
-        <FaLinkedin className="mt-3 size-6" />
-      </a>
+      <Image
+        src={imageSrc}
+        alt=""
+        className="mt-8 size-52 rounded-full lg:size-[200px]"
+      />
+      <h3 className="mt-2 text-2xl font-bold md:text-3xl">{name}</h3>
+      <p className="mt-1">{role}</p>
+      <Link href={linkedInURL ?? ""}>
+        <FaLinkedin className="mb-4 mt-[6px] size-6" />
+      </Link>
     </div>
   );
 };

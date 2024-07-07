ce09fbf2dae0c3dedb2647b7bb232bbcfd6f6795
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-07-07 18:41:44 +0530

    added link preview

--- src/components/Cittic.jsx
+++ src/components/Cittic.jsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { Poppins, Saira } from "next/font/google";
+// import { motion } from "framer-motion";
+import { LinkPreview } from "@/components/ui/link-preview";
 
 const saira = Saira({
   weight: "700",
@@ -12,39 +14,52 @@ const poppins = Poppins({
   weight: "400",
   subsets: ["latin"],
 });
-function Info_text() {
+
+const InfoText = () => {
   return (
     <div className="flex flex-col space-x-2 text-[#FFFFFF]">
-      <h className={"antialised text-[40px] leading-[56px]" + saira}>
+      <h1
+        className={"antialised text-[40px] leading-[56px] " + saira.className}
+      >
         {" "}
-        CITTIC LOGO
-      </h>
-      <p className={"antialised text-[16px] leading-[26px]" + poppins}>
-        CITTIC CUSAT provide a springboard to budding entreprenurs who wish to
-        launch themselves into the world of technology based business carrers.
-        In CITTIC, bright ideas can be developed to a product or service using
+        CITTIC
+      </h1>
+      <p
+        className={"antialised text-[16px] leading-[26px] " + poppins.className}
+      >
+        <LinkPreview
+          url="https://cittic.cusat.ac.in/"
+          className="font-bold text-white"
+        >
+          CITTIC CUSAT
+        </LinkPreview>{" "}
+        provide a springboard to budding entrepreneurs who wish to launch
+        themselves into the world of technology-based business careers. In
+        CITTIC, bright ideas can be developed into a product or service using
         advanced technology solutions. This innovation centre is designed to
         provide all the support to make business ventures successful.
       </p>
     </div>
   );
-}
+};
 
-export default function Cittic() {
+const Cittic = () => {
   return (
     <div className="flex flex-col space-y-1 bg-[#003C43] px-[10vw] py-[8vw] lg:h-screen lg:flex-row">
       <div className="flex bg-[#003C43] md:w-1/2 md:items-center md:justify-center">
-        <Info_text />
+        <InfoText />
       </div>
       <div className="flex w-full bg-[#003C43]">
         <Image
           width={1500}
           height={1500}
           src="/images/citticlogo.svg"
-          className="bg-[#003C43] "
+          className="bg-[#003C43]"
           alt="Cittic Logo"
         />
       </div>
     </div>
   );
-}
+};
+
+export default Cittic;
